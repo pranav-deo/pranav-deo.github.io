c7b7cdc92821ba847311170a154acbbc6e860f06
pranav-deo <pranavdeo99@gmail.com>
2025-12-06 04:26:30 +0900

    fix paths

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // <--- ADD THIS LINE (make sure index.css exists in src root)
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
